2e6adccf36d05ba349d12d34531f809bc390a982
abdulrauf <abdul.rauf@coderzhunt.com>
2023-11-11 15:48:41 +0500

    make user login and register apis

---
+++ Tour Management System/Controllers/AuthController.cs
@@ -0,0 +1,134 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Tour_Management_System.Models;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Tour_Management_System.DTOs;
+
+
+namespace Tour_Management_System.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        public static User user = new User();
+        private readonly DataContext _context;
+        private readonly IConfiguration _configuration;
+
+
+        public AuthController(DataContext context, IConfiguration configuration)
+        {
+            _context = context;
+            _configuration = configuration;
+        }
+       
+        [HttpPost("register")]
+        public async Task<ActionResult<User>> Register(User request)
+        {
+            try
+            {
+                // Validation checks
+                if (request == null)
+                {
+                    return BadRequest("User data is null.");
+                }
+
+                if (string.IsNullOrWhiteSpace(request.Name) || string.IsNullOrWhiteSpace(request.Email) || string.IsNullOrWhiteSpace(request.PasswordHash))
+                {
+                    return BadRequest("Name, Email, and Password are required.");
+                }
+
+                string passwordHash
+                = BCrypt.Net.BCrypt.HashPassword(request.PasswordHash);
+
+                user.Name = request.Name;
+                user.Email = request.Email;
+                user.PasswordHash = passwordHash;
+                _context.Users.Add(user);
+                await _context.SaveChangesAsync();
+
+                return Ok("User Created");
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] UserLoginDTO request)
+        {
+            try
+            {
+                // Validation checks
+                if (request == null)
+                {
+                    return BadRequest("User data is null.");
+                }
+
+                if (string.IsNullOrWhiteSpace(request.Email) || string.IsNullOrWhiteSpace(request.PasswordHash))
+                {
+                    return BadRequest("Email and Password are required.");
+                }
+
+                var getUser = await _context.Users.FirstOrDefaultAsync(u => u.Email == request.Email);
+
+                if (getUser == null)
+                {
+                    return NotFound("User Not Found");
+                }
+
+                // Check the password (replace this with your actual password verification logic)
+                if (!BCrypt.Net.BCrypt.Verify(request.PasswordHash, getUser.PasswordHash))
+                {
+                    return BadRequest("Incorrect Password");
+                }
+                //return Ok(getUser);
+                // Authentication successful
+                var token = GenerateToken(getUser);
+                var response = new TokenResponseDTO
+                {
+                    Token = token
+                };
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+
+        private string GenerateToken(User user)
+        {
+            var issuer = _configuration["Jwt:Issuer"];
+            var audience = _configuration["Jwt:Audience"];
+            var key = Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]);
+            var signingCredentials = new SigningCredentials(
+                            new SymmetricSecurityKey(key),
+                            SecurityAlgorithms.HmacSha512Signature
+                        );
+            var subject = new ClaimsIdentity(new[]
+            {
+              new Claim(ClaimTypes.Email, user.Email),
+              new Claim(ClaimTypes.Name, user.Name),
+              new Claim(ClaimTypes.Role, user.Role),
+            });
+            var expires = DateTime.UtcNow.AddMinutes(10);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = subject,
+                Expires = DateTime.UtcNow.AddMinutes(10),
+                Issuer = issuer,
+                Audience = audience,
+                SigningCredentials = signingCredentials
+            };
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            var jwtToken = tokenHandler.WriteToken(token);
+            return jwtToken;
+      }
+
+        }
+    }
---
+++ Tour Management System/Controllers/UserController.cs
@@ -0,0 +1,110 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Tour_Management_System.DTOs;
+using Tour_Management_System.Models;
+
+namespace Tour_Management_System.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        public static User user = new User();
+        private readonly DataContext _context;
+
+
+        public UserController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [Authorize(Roles = "Admin")]
+        [HttpGet]
+        [Route("Users")]
+        public async Task<ActionResult<User>> GetAllUsers()
+        {
+            try
+            {
+                var users = await _context.Users.ToListAsync();
+
+                if (users == null || users.Count == 0)
+                {
+                    return NotFound("No users found.");
+                }
+
+                return Ok(users);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+
+        [HttpPut("updateUserPassword")]
+        public async Task<ActionResult<User>> UpdateUserPassword(PasswordUpdateDTO request)
+        {
+            var dbUser = await _context.Users.FirstOrDefaultAsync(u => u.Email == request.Email);
+
+            if (dbUser == null)
+                return BadRequest("User not found.");
+
+            if (string.IsNullOrWhiteSpace(request.OldPassword) || string.IsNullOrWhiteSpace(request.NewPassword))
+            {
+                return BadRequest("Old Password and New Password are required.");
+            }
+
+            if (request.OldPassword == request.NewPassword)
+            {
+                return BadRequest("Old Password and New Password should be different.");
+            }
+
+            if (!BCrypt.Net.BCrypt.Verify(request.OldPassword, dbUser.PasswordHash))
+            {
+                return BadRequest("Incorrect Password");
+            }
+
+            if (request.NewPassword != null)
+            {
+                string passwordHash = BCrypt.Net.BCrypt.HashPassword(request.NewPassword);
+                dbUser.PasswordHash = passwordHash;
+            }
+
+            dbUser.Email = request.Email;
+            await _context.SaveChangesAsync();
+
+            return Ok("Password has been Updated");
+        }
+
+        [Authorize(Roles = "Admin")]
+        [HttpPut("updateUser/{id}")]
+        public async Task<ActionResult<User>> UpdateUser(UserLoginDTO request, [FromRoute] int id)
+        {
+            var dbUser = await _context.Users.FindAsync(id);
+
+            if (dbUser == null)
+                return BadRequest("User not found.");
+            string passwordHash = BCrypt.Net.BCrypt.HashPassword(request.PasswordHash);
+           
+
+            dbUser.Email = request.Email;
+            dbUser.PasswordHash = passwordHash;
+            await _context.SaveChangesAsync();
+
+            return Ok("User Updated");
+        }
+
+        [Authorize(Roles = "Admin")]
+        [HttpDelete("deleteUser/{id}")]
+        public async Task<ActionResult<List<User>>> DeleteUser(int id)
+        {
+            var dbUser = await _context.Users.FindAsync(id);
+            if (dbUser == null)
+                return BadRequest("User not found.");
+
+            _context.Users.Remove(dbUser);
+            await _context.SaveChangesAsync();
+
+            return Ok(await _context.Users.ToListAsync());
+        }
+    }
+}
